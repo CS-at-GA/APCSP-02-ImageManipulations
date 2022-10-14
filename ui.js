5ab6a192a4766a8901676e2ee0a4bf28a0951c3a
Mr. Oswald <62300716+gajoswald@users.noreply.github.com>
2022-10-14 11:13:50 -0400

    dragInfo change was made on replit and not here. Updated now.

--- ui.js
+++ ui.js
@@ -2,6 +2,7 @@ const utilBindings = {
   "o":copyOfOriginal
 }
 
+// add new filters and key bindings here
 const editBindings = {
   "b":simpleBlackAndWhite,
   "l":simpleBlur,
@@ -25,15 +26,29 @@ function keyPressed() {
 
 function mousePressed() {
   loop();
-  dragInfo = {...dragInfo, startX: mouseX, startY: mouseY }
+  dragInfo = {startX: mouseX, startY: mouseY }
 }
 
 function mouseDragged() {
   dragInfo = {...dragInfo, endX: mouseX, endY: mouseY }
 }
 
-function mouseReleased() {
+function mouseReleased() { 
   dragInfo = {...dragInfo, endX: mouseX, endY: mouseY }
+  if( dragInfo.startX > dragInfo.endX && dragInfo.startY < dragInfo.endY ) {
+    [dragInfo.startX, dragInfo.endX] = [dragInfo.endX, dragInfo.startX]
+  } else if( dragInfo.startX < dragInfo.endX && dragInfo.startY > dragInfo.endY ) {
+    [dragInfo.startY, dragInfo.endY] = [dragInfo.endY, dragInfo.startY]
+  } else if( dragInfo.startX > dragInfo.endX && dragInfo.startY > dragInfo.endY ) {
+    // I will admit to not understanding why the 
+    // above destructuring approach doesn't work here
+    tempX = dragInfo.startX;
+    tempY = dragInfo.startY;
+    dragInfo.startX = dragInfo.endX;
+    dragInfo.startY = dragInfo.endY;
+    dragInfo.endX = tempX;
+    dragInfo.endY = tempY;
+  }
   noLoop();
 }
 
